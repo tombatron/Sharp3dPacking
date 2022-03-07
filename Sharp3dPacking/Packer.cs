a71a7efa90754392bc738a602e7db3826a76fbf8
Tommy Hanks <tommy.hanks@gmail.com>
2022-03-07 07:01:10 -0500

    Some documentation as well as a minor allocation fix.

--- Sharp3dPacking/Packer.cs
+++ Sharp3dPacking/Packer.cs
@@ -2,39 +2,118 @@
 
 namespace Sharp3dPacking;
 
+/// <summary>
+/// This class takes a collection of bins and items and attempts to fit the items into the bins.
+/// </summary>
 public class Packer
 {
-    private List<Bin> _bins = new();
-    private List<Item> _items = new();
-    private int _unfitItems = 0;
+    private readonly List<Bin> _bins = new();
+    private readonly List<Item> _items = new();
     private int _totalItems;
 
+    /// <summary>
+    /// A read-only collection of bins that have been associated with this packer. 
+    /// </summary>
     public ReadOnlyCollection<Bin> Bins => new(_bins);
 
+    /// <summary>
+    /// A read-only collection of the items that have been associated with this packer. 
+    /// </summary>
     public ReadOnlyCollection<Item> Items => new(_items);
 
+    /// <summary>
+    /// The total number of items assigned to this packer. 
+    /// </summary>
     public int TotalItems => _totalItems;
 
-    public int UnfitItems => _unfitItems;
+    /// <summary>
+    /// The total number of items that could not be fit within a provided container.
+    /// </summary>
+    public int UnfitItems { get; } = 0;
 
-    public void AddBin(Bin bin) => _bins.Add(bin);
+    /// <summary>
+    /// Add a bin to the packer.
+    /// </summary>
+    /// <param name="bin">An initialized instance of the `Bin` class.</param>
+    public void AddBin(Bin bin)
+    {
+        if (_bins.Count == 0)
+        {
+            _bins.Add(bin);
+            return;
+        }
 
+        for (var i = 0; i < _bins.Count; i++)
+        {
+            if (bin.Volume >= _bins[i].Volume)
+            {
+                _bins.Insert(i, bin);
+                return;
+            }
+        }
+
+        _bins.Add(bin);
+    }
+
+    /// <summary>
+    /// Add a bin to the packer by providing initialization values (instead of an instance) of the bin.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <param name="depth"></param>
+    /// <param name="maximumWeightCapacity"></param>
     public void AddBin(string name, decimal width, decimal height, decimal depth, decimal maximumWeightCapacity) =>
         AddBin(new Bin(name, width, height, depth, maximumWeightCapacity));
 
+    /// <summary>
+    /// Add multiple bins to the packer by providing a collection of initialized bins.
+    /// </summary>
+    /// <param name="bins"></param>
     public void AddBins(IEnumerable<Bin> bins) =>
         _bins.AddRange(bins);
 
+    /// <summary>
+    /// Add an item to the packer
+    /// </summary>
+    /// <param name="item">An initialized instance of the `Item` class.</param>
     public void AddItem(Item item)
     {
         _totalItems++;
 
+        if (_items.Count == 0)
+        {
+            _items.Add(item);
+            return;
+        }
+
+        for (var i = 0; i < _items.Count; i++)
+        {
+            if (item.Volume >= _items[i].Volume)
+            {
+                _items.Insert(i, item);
+                return;
+            }
+        }
+
         _items.Add(item);
     }
 
+    /// <summary>
+    /// Add a item to the packer by providing initialization values (instead of an instance) of the item.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <param name="depth"></param>
+    /// <param name="weight"></param>
     public void AddItem(string name, decimal width, decimal height, decimal depth, decimal weight) =>
         AddItem(new Item(name, width, height, depth, weight));
 
+    /// <summary>
+    /// Add multiple items to the packer by providing a collection of initialized items.
+    /// </summary>
+    /// <param name="items"></param>
     public void AddItems(IEnumerable<Item> items)
     {
         foreach (var item in items)
@@ -42,23 +121,34 @@ public class Packer
             AddItem(item);
         }
     }
-    
+
+    /// <summary>
+    /// Start packing!
+    /// </summary>
+    /// <param name="biggerFirst">Should the algorithm attempt to pack larger items first? (false by default)</param>
+    /// <param name="distributeItems">Should the algorithm attempt to distribute items across multiple available bins? (false by default)</param>
     public void Pack(bool biggerFirst = false, bool distributeItems = false)
     {
-        var sortedBins = (biggerFirst ? _bins.OrderByDescending(x => x.Volume) : _bins.OrderBy(x => x.Volume)).ToList();
+        var itemStartIndex = biggerFirst ? _items.Count - 1 : 0;
+        var itemIncrement = biggerFirst ? -1 : 1;
+
+        var binStartIndex = biggerFirst ? _items.Count - 1 : 0;
+        var binIncrement = biggerFirst ? -1 : 1;
+
+        var packed = new bool[_items.Count];
         
-        var sortedItems = (biggerFirst ? _items.OrderByDescending(x=>x.Volume) : _items.OrderBy(x=>x.Volume)).ToList();
-
-        foreach (var bin in sortedBins)
+        for (var bi = binStartIndex; (bi < _bins.Count && bi >= 0); bi += binIncrement)
         {
-            foreach (var item in sortedItems)
+            for (var ii = itemStartIndex; (ii < _items.Count && ii >= 0); ii += itemIncrement)
             {
-                PackToBin(bin, item);
-            }
+                if (distributeItems && packed[ii])
+                {
+                    continue;
+                }
+                
+                PackToBin(_bins[bi], _items[ii]);
 
-            if (distributeItems)
-            {
-                sortedItems = sortedItems.Where(x => !bin.Items.Contains(x)).ToList();
+                packed[ii] = _bins[bi].Items.Contains(_items[ii]);
             }
         }
     }
